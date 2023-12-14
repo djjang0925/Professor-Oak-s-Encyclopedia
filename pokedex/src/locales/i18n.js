a608df98c59aaa50f76c7f7df6901c2c8535e64a
뚱이 <djjang0925@gmail.com>
2023-12-14 22:13:36 +0900

    translation

--- pokedex/src/locales/i18n.js
+++ pokedex/src/locales/i18n.js
@@ -16,7 +16,7 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: "ko", // 기본 설정 언어, 'cimode'로 설정할 경우 키 값으로 출력된다.
+    lng: "en", // 기본 설정 언어, 'cimode'로 설정할 경우 키 값으로 출력된다.
     fallbackLng: "en", // 번역 파일에서 찾을 수 없는 경우 기본 언어
     interpolation: {
       escapeValue: false
